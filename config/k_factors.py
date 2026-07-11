ffddf5dcec820a60554d6d4744855a66bc69f3a7
Bhavya Arora <bhavya010arora@gmail.com>
2026-07-11 14:42:02 +0530

    Add tournament K-factor configuration

---
+++ config/k_factors.py
@@ -0,0 +1,31 @@
+K_FACTORS = {
+    "Friendly": 20,
+    "FIFA World Cup": 60,
+    "FIFA World Cup qualification": 40,
+    "UEFA Euro": 50,
+    "Copa América": 50,
+    "African Cup of Nations": 50,
+    "AFC Asian Cup": 50,
+    "Gold Cup": 50,
+    "Oceania Nations Cup": 50,
+
+    "UEFA Euro qualification": 40,
+    "African Cup of Nations qualification": 40,
+    "AFC Asian Cup qualification": 40,
+    "Gold Cup qualification": 40,
+
+    "UEFA Nations League": 35,
+    "CONCACAF Nations League": 35,
+
+    "AFF Championship": 30,
+    "ASEAN Championship": 30,
+    "Gulf Cup": 30,
+    "SAFF Cup": 30,
+    "EAFF Championship": 30,
+    "WAFF Championship": 30,
+    "UNCAF Cup": 30,
+    "CECAFA Cup": 30,
+    "COSAFA Cup": 30,
+    "CFU Caribbean Cup": 30,
+    "Arab Cup": 30,
+}
